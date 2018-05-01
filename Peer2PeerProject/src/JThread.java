7fb4cc3decc493286282be7da031412b1276e426
NinjaCoder8 <charbel.dawoud@gmail.com>
2018-05-01 09:28:27 +0300

    peer2peer part 1 finalzizing

--- Peer2PeerProject/src/JThread.java
+++ Peer2PeerProject/src/JThread.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class JThread implements Runnable {
    private Thread t;
    private int threadID;
+   private Peer peer;
    private String threadName;
    
    public JThread(String name, int ID) {
@@ -15,15 +18,15 @@ public class JThread implements Runnable {
    public void run(){
       System.out.println("Running " +  threadName );
 		try {
-			Peer p = new Peer("localhost", (2000 + threadID));
+			Peer p = new Peer("localhost", threadID);
+			peer = p;
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 	}
       try {
-         for(int i = 4; i > 0; i--) {
+         while(true){
             //System.out.println("Thread: " + threadName + ", " + i);
-            // Let the thread sleep for a while.
 
             Thread.sleep(50);
          }
@@ -40,4 +43,23 @@ public class JThread implements Runnable {
          t.start ();
       }
    }
+
+	public void connectPeer(int port, int max) throws UnknownHostException, IOException {
+		 if(this.peer.canEstablishConnections()){
+	         InetAddress address = InetAddress.getByName("localhost");
+	         Socket socket = new Socket(address, port);
+	         this.peer.incrementConnections(port, max);
+	         System.out.println("Connection established between port " + threadID + " and port " + port);
+		 }else{
+	         System.out.println("Maximum Connections Reached For Peer on Port " + threadID);
+		 }
+	}
+	
+	public Peer getPeer(){
+		return this.peer;
+	}
+	
+	public int getPeerID(){
+		return this.threadID;
+	}
 }
